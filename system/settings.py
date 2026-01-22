0678318f9ac62e8d83d1ee6cfd272088166aac62
m1amineratit <ratitamine3@gmail.com>
2026-01-22 13:52:08 +0100

    add backend configuration

--- system/settings.py
+++ system/settings.py
@@ -240,3 +240,13 @@ LOGGING = {
         },
     },
 }
+
+
+# EMAIL BACKEND SETTINGS
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'amineratit6@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
