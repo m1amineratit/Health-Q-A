a40fcee181fc22ed036ca19e32e420671a7af3a4
m1amineratit <ratitamine3@gmail.com>
2026-01-24 18:54:08 +0100

    add answered questions

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('api/questions/<uuid:question_id>/', json_api.get_question_detail_api, name='api_get_question_detail'),
     path('api/questions/<uuid:question_id>/answer/', json_api.submit_answer_api, name='api_submit_answer'),
     path('api/feed/answered-questions/', json_api.answered_questions_feed_api, name='api_answered_questions_feed'),
-    path('api/feed/unanswered-questions/', json_api.answered_questions_feed_api, name='api_unanswered_questions_feed'),
+    path('api/feed/answered-questions/', json_api.answered_questions_feed_api, name='api_unanswered_questions_feed'),
 ]
 
