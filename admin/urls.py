d711ed6b4fa2e96439dad3e96f4c9f3b202e9e0c
m1amineratit <ratitamine3@gmail.com>
2026-01-25 21:30:01 +0100

    edit and fix admin login code

--- admin/urls.py
+++ admin/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', views.admin_login_view, name='admin_login'),
+    path('admin', views.admin_login_view, name='admin_login'),
 ]
